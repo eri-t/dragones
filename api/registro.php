59a146cf2f847b72b301eafa6d94b00bc444b5d0
Saphira1406 <mellone.flore@gmail.com>
2021-05-16 18:45:29 -0300

    registro cwm

---
+++ api/registro.php
@@ -0,0 +1,38 @@
+<?php
+
+require '../autoload.php';
+
+header("Content-Type: application/json");
+
+$db = mysqli_connect('localhost', 'root', '', 'dragones');
+switch ($_SERVER['REQUEST_METHOD']) {
+    case 'POST':
+
+    $inputData = file_get_contents('php://input');
+    $postData = json_decode($inputData, true);
+
+    $usuario = $postData['usuario'];
+    $email = $postData['email'];
+    $password = password_hash($postData['password'], PASSWORD_DEFAULT);
+
+
+   $query = "INSERT INTO usuarios (email, usuario, password)
+                VALUES ('{$email}', '{$usuario}', '{$password}')";
+
+    $exito = mysqli_query($db, $query);
+
+
+        if ($exito) {
+            echo json_encode([
+                'success' => true,
+                'msg' => 'El usuario se agregó con éxito.',
+            ]);
+        } else {
+            echo json_encode([
+                'success' => false,
+                'msg' => 'Ocurrio un error al tratar de agregar el usuario',
+            ]);
+
+        }
+        break;
+}
